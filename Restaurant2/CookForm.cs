a83141661dce54e4dcdaf9567e6ea3dac5c79508
mlhomfeld <mlhomfeld@yahoo.com>
2017-04-25 19:57:57 -0500

    Skeleton of restaurant project

--- Restaurant2/CookForm.cs
+++ Restaurant2/CookForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Restaurant2
 {
-    public partial class Form1 : Form
+    public partial class CookForm : Form
     {
-        public Form1()
+        public CookForm()
         {
             InitializeComponent();
         }
