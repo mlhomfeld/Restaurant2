0494ce5c6997ad3b0ec14c8097102467e6042a80
Ray Rigoni <rayjrig@gmail.com>
2017-05-02 15:38:02 -0500

    Made buttons for the host form.

--- Restaurant2/HostForm.Designer.cs
+++ Restaurant2/HostForm.Designer.cs
@@ -28,11 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnTable1 = new System.Windows.Forms.Button();
+            this.btnTable2 = new System.Windows.Forms.Button();
+            this.btnTable3 = new System.Windows.Forms.Button();
+            this.btnTable4 = new System.Windows.Forms.Button();
+            this.btnTable5 = new System.Windows.Forms.Button();
+            this.btnTable6 = new System.Windows.Forms.Button();
+            this.btnTable7 = new System.Windows.Forms.Button();
+            this.btnCreateParty = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTable1
+            // 
+            this.btnTable1.Location = new System.Drawing.Point(105, 65);
+            this.btnTable1.Name = "btnTable1";
+            this.btnTable1.Size = new System.Drawing.Size(101, 78);
+            this.btnTable1.TabIndex = 0;
+            this.btnTable1.Text = "Table 1";
+            this.btnTable1.UseVisualStyleBackColor = true;
+            // 
+            // btnTable2
+            // 
+            this.btnTable2.Location = new System.Drawing.Point(105, 211);
+            this.btnTable2.Name = "btnTable2";
+            this.btnTable2.Size = new System.Drawing.Size(101, 81);
+            this.btnTable2.TabIndex = 1;
+            this.btnTable2.Text = "Table 2";
+            this.btnTable2.UseVisualStyleBackColor = true;
+            // 
+            // btnTable3
+            // 
+            this.btnTable3.Location = new System.Drawing.Point(329, 65);
+            this.btnTable3.Name = "btnTable3";
+            this.btnTable3.Size = new System.Drawing.Size(102, 78);
+            this.btnTable3.TabIndex = 2;
+            this.btnTable3.Text = "Table 3";
+            this.btnTable3.UseVisualStyleBackColor = true;
+            // 
+            // btnTable4
+            // 
+            this.btnTable4.Location = new System.Drawing.Point(329, 211);
+            this.btnTable4.Name = "btnTable4";
+            this.btnTable4.Size = new System.Drawing.Size(102, 81);
+            this.btnTable4.TabIndex = 3;
+            this.btnTable4.Text = "Table 4";
+            this.btnTable4.UseVisualStyleBackColor = true;
+            // 
+            // btnTable5
+            // 
+            this.btnTable5.Location = new System.Drawing.Point(552, 38);
+            this.btnTable5.Name = "btnTable5";
+            this.btnTable5.Size = new System.Drawing.Size(93, 80);
+            this.btnTable5.TabIndex = 4;
+            this.btnTable5.Text = "Table 5";
+            this.btnTable5.UseVisualStyleBackColor = true;
+            // 
+            // btnTable6
+            // 
+            this.btnTable6.Location = new System.Drawing.Point(552, 160);
+            this.btnTable6.Name = "btnTable6";
+            this.btnTable6.Size = new System.Drawing.Size(93, 68);
+            this.btnTable6.TabIndex = 5;
+            this.btnTable6.Text = "Table 6";
+            this.btnTable6.UseVisualStyleBackColor = true;
+            // 
+            // btnTable7
+            // 
+            this.btnTable7.Location = new System.Drawing.Point(552, 273);
+            this.btnTable7.Name = "btnTable7";
+            this.btnTable7.Size = new System.Drawing.Size(93, 60);
+            this.btnTable7.TabIndex = 6;
+            this.btnTable7.Text = "Table 7";
+            this.btnTable7.UseVisualStyleBackColor = true;
+            // 
+            // btnCreateParty
+            // 
+            this.btnCreateParty.Location = new System.Drawing.Point(777, 38);
+            this.btnCreateParty.Name = "btnCreateParty";
+            this.btnCreateParty.Size = new System.Drawing.Size(148, 91);
+            this.btnCreateParty.TabIndex = 7;
+            this.btnCreateParty.Text = "Create Party";
+            this.btnCreateParty.UseVisualStyleBackColor = true;
+            // 
+            // HostForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 507);
+            this.Controls.Add(this.btnCreateParty);
+            this.Controls.Add(this.btnTable7);
+            this.Controls.Add(this.btnTable6);
+            this.Controls.Add(this.btnTable5);
+            this.Controls.Add(this.btnTable4);
+            this.Controls.Add(this.btnTable3);
+            this.Controls.Add(this.btnTable2);
+            this.Controls.Add(this.btnTable1);
+            this.Name = "HostForm";
             this.Text = "HostForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnTable1;
+        private System.Windows.Forms.Button btnTable2;
+        private System.Windows.Forms.Button btnTable3;
+        private System.Windows.Forms.Button btnTable4;
+        private System.Windows.Forms.Button btnTable5;
+        private System.Windows.Forms.Button btnTable6;
+        private System.Windows.Forms.Button btnTable7;
+        private System.Windows.Forms.Button btnCreateParty;
     }
 }
