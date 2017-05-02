e14bd4600d1394015257e1c5b0ecb099796af426
daywahsi <d0525808@amarillcollege.com>
2017-05-02 15:59:29 -0500

    I was here again

--- Restaurant2/Customer.cs
+++ Restaurant2/Customer.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data;
+
 namespace Restaurant2
 {
     public class Customer
     {
-        private string groupName;
-        private int groupSize;
-        private int groupTable;
+        SqlConnection connection = new SqlConnection();
+
+            //Connection.ConnectionString = "Server=cis1.actx.edu;Database=Restaurant2;User Id=db1;Password = db10;";
+            //connection.Open();
+            //Console.WriteLine(connection.ServerVersion);
+            //Console.ReadKey();
+
+            //Console.WriteLine("---SQL INSERT---");
+            //using (SqlCommand insertNewProfessor = connection.CreateCommand())
+            //{
+            //    insertNewProfessor.CommandText = "insert into dbo.Professor values (@FirstName, @LastName, @Degree);";
+            //    var degreeParam = new SqlParameter("Degree", SqlDbType.VarChar) { Value = "M.S. in IS" };
+            //    var fnameParam = new SqlParameter("FirstName", SqlDbType.VarChar) { Value = "Dewayne" };
+            //    var lnameParam = new SqlParameter("LastName", SqlDbType.VarChar) { Value = "Higgs" };
+            //    insertNewProfessor.Parameters.Add(degreeParam);
+            //    insertNewProfessor.Parameters.Add(fnameParam);
+            //    insertNewProfessor.Parameters.Add(lnameParam);
+            //    insertNewProfessor.ExecuteNonQuery();
+            //}
+            //Console.ReadKey();
+        //private string groupName;
+        //private int groupSize;
+        //private int groupTable;
         private decimal groupTotal;
 
         public void Pay (decimal Payment )
