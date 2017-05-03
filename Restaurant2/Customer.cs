6685e36a686780f83aa02c34556aec4562540ab1
daywahsi <d0525808@amarillocollege.com>
2017-05-02 19:59:45 -0500

    added codes in customers

--- Restaurant2/Customer.cs
+++ Restaurant2/Customer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Data.Sql;
 using System.Data;
@@ -11,53 +11,74 @@ namespace Restaurant2
 {
     public class Customer
     {
-        SqlConnection connection = new SqlConnection();
+        // SqlConnection connection = new SqlConnection();
 
-            //Connection.ConnectionString = "Server=cis1.actx.edu;Database=Restaurant2;User Id=db1;Password = db10;";
-            //connection.Open();
-            //Console.WriteLine(connection.ServerVersion);
-            //Console.ReadKey();
+        //Connection.ConnectionString = "Server=cis1.actx.edu;Database=Restaurant2;User Id=db1;Password = db10;";
+        //connection.Open();
+        //Console.WriteLine(connection.ServerVersion);
+        //Console.ReadKey();
 
-            //Console.WriteLine("---SQL INSERT---");
-            //using (SqlCommand insertNewProfessor = connection.CreateCommand())
-            //{
-            //    insertNewProfessor.CommandText = "insert into dbo.Professor values (@FirstName, @LastName, @Degree);";
-            //    var degreeParam = new SqlParameter("Degree", SqlDbType.VarChar) { Value = "M.S. in IS" };
-            //    var fnameParam = new SqlParameter("FirstName", SqlDbType.VarChar) { Value = "Dewayne" };
-            //    var lnameParam = new SqlParameter("LastName", SqlDbType.VarChar) { Value = "Higgs" };
-            //    insertNewProfessor.Parameters.Add(degreeParam);
-            //    insertNewProfessor.Parameters.Add(fnameParam);
-            //    insertNewProfessor.Parameters.Add(lnameParam);
-            //    insertNewProfessor.ExecuteNonQuery();
-            //}
-            //Console.ReadKey();
+        //Console.WriteLine("---SQL INSERT---");
+        //using (SqlCommand insertNewProfessor = connection.CreateCommand())
+        //{
+        //    insertNewProfessor.CommandText = "insert into dbo.Professor values (@FirstName, @LastName, @Degree);";
+        //    var degreeParam = new SqlParameter("Degree", SqlDbType.VarChar) { Value = "M.S. in IS" };
+        //    var fnameParam = new SqlParameter("FirstName", SqlDbType.VarChar) { Value = "Dewayne" };
+        //    var lnameParam = new SqlParameter("LastName", SqlDbType.VarChar) { Value = "Higgs" };
+        //    insertNewProfessor.Parameters.Add(degreeParam);
+        //    insertNewProfessor.Parameters.Add(fnameParam);
+        //    insertNewProfessor.Parameters.Add(lnameParam);
+        //    insertNewProfessor.ExecuteNonQuery();
+        //}
+        //Console.ReadKey();
+
         //private string groupName;
         //private int groupSize;
         //private int groupTable;
+
+
+        //take the payment and subtract to the grouptotal;
+        //if they didn't pay enough keep subtrace.
+
         private decimal groupTotal;
+       
 
         public void Pay (decimal Payment )
         {
-            while(true)
+
+            decimal remainingBalance = groupTotal - Payment;
+            //if the customers pay more then they own
+            if(remainingBalance < 0)
             {
-                if(Payment >= groupTotal)
-                {
-                    Console.WriteLine("You payment is not enough ");
 
-                    break;
-                
-                }
-                else
-                {
+                MessageBox.Show("You change is " + remainingBalance.ToString());
+            }
+            //if the customers pay less then they own
+            else if(remainingBalance > 0)
+            {
+
+                    remainingBalance = groupTotal - Payment;
+                    MessageBox.Show("You own " + remainingBalance.ToString());
+              
+
+            }
+            else
+
+            if (Payment >=  groupTotal)
+            {
                     groupTotal = groupTotal - Payment;
-                    Console.WriteLine("Thank you");
-                    //groupTotal = groupTotal;
-                }
-                    //take the payment and subtrace to the grouptotal;
-                    //if they didn't pay enough keep subtrace.
-             
-             }
+                    MessageBox.Show("Your change is " + groupTotal );
+                    
 
+                    //break;
+                
+            }
+            else
+            {
+                    groupTotal = groupTotal - Payment;
+                    MessageBox.Show("Thank you");
+                    
+            }
         }
     }
 }
