9ded001d8a246675e8f10b895f2b261a3c39c7f2
Ray <Rayjrig@gmail.com>
2017-05-02 19:55:12 -0500

    Added the databse code to the host class instead of the hostform

--- Restaurant2/Host.cs
+++ Restaurant2/Host.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Data;
 
 namespace Restaurant2
 {
@@ -17,11 +19,44 @@ namespace Restaurant2
 
         }
 
-        public void SetTableToOccupied()
+        public void SetTableToOccupied(int tableNumber)
         {
-            throw new System.NotImplementedException();
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to set this table to Occupied?", "!!!", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                try
+                {
+
+                    SqlConnection connection = new SqlConnection();
+                    connection.ConnectionString = "Server=cis1.actx.edu;Database=Project2;User Id=db2;Password = db20;";
+                    connection.Open();
+                    using (SqlCommand updateSeatingArea = connection.CreateCommand())
+                    {
+                        updateSeatingArea.CommandText = "update dbo.SeatingArea set SeatingAreaStatus = @SeatingAreaStatus where SeatingAreaID = @SeatingAreaID;";
+                        var seatingAreaParam = new SqlParameter("SeatingAreaStatus", SqlDbType.VarChar) { Value = "Occupied" };
+                        var idParam = new SqlParameter("ID", SqlDbType.Int) { Value = tableNumber };
+                        updateSeatingArea.Parameters.Add(seatingAreaParam);
+                        updateSeatingArea.Parameters.Add(idParam);
+
+                        updateSeatingArea.ExecuteNonQuery();
+
+                    }
+                    MessageBox.Show("Table 1 has been set to Occupied");
+                    connection.Close();
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+            }
 
         }
+
+    }
 
         public void RemoveFromQueue()
         {
