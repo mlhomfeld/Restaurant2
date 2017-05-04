6b2b58daea23d73a1a2d25dfe482a415e4a2950e
hedge <hector.jacquez@outlook.com>
2017-05-04 18:41:38 -0500

    yes

--- Restaurant2/ManagerForm.Designer.cs
+++ Restaurant2/ManagerForm.Designer.cs
@@ -28,17 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnCreateEmployee = new System.Windows.Forms.Button();
             this.btn = new System.Windows.Forms.Button();
             this.btnModifyEmployee = new System.Windows.Forms.Button();
             this.btnLogout = new System.Windows.Forms.Button();
+            this.txtRmvEmployee = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.EmployeeID = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnCreateEmployee
             // 
-            this.btnCreateEmployee.Location = new System.Drawing.Point(12, 27);
+            this.btnCreateEmployee.Location = new System.Drawing.Point(8, 18);
+            this.btnCreateEmployee.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCreateEmployee.Name = "btnCreateEmployee";
-            this.btnCreateEmployee.Size = new System.Drawing.Size(138, 56);
+            this.btnCreateEmployee.Size = new System.Drawing.Size(92, 36);
             this.btnCreateEmployee.TabIndex = 0;
             this.btnCreateEmployee.Text = "Create Employee";
             this.btnCreateEmployee.UseVisualStyleBackColor = true;
@@ -46,18 +51,20 @@
             // 
             // btn
             // 
-            this.btn.Location = new System.Drawing.Point(12, 89);
+            this.btn.Location = new System.Drawing.Point(8, 58);
+            this.btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn.Name = "btn";
-            this.btn.Size = new System.Drawing.Size(138, 55);
+            this.btn.Size = new System.Drawing.Size(92, 36);
             this.btn.TabIndex = 1;
             this.btn.Text = "Remove Employee";
             this.btn.UseVisualStyleBackColor = true;
             // 
             // btnModifyEmployee
             // 
-            this.btnModifyEmployee.Location = new System.Drawing.Point(12, 150);
+            this.btnModifyEmployee.Location = new System.Drawing.Point(8, 97);
+            this.btnModifyEmployee.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnModifyEmployee.Name = "btnModifyEmployee";
-            this.btnModifyEmployee.Size = new System.Drawing.Size(138, 54);
+            this.btnModifyEmployee.Size = new System.Drawing.Size(92, 35);
             this.btnModifyEmployee.TabIndex = 2;
             this.btnModifyEmployee.Text = "Modify Employee";
             this.btnModifyEmployee.UseVisualStyleBackColor = true;
@@ -65,26 +72,53 @@
             // 
             // btnLogout
             // 
-            this.btnLogout.Location = new System.Drawing.Point(12, 210);
+            this.btnLogout.Location = new System.Drawing.Point(8, 136);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(138, 56);
+            this.btnLogout.Size = new System.Drawing.Size(92, 36);
             this.btnLogout.TabIndex = 3;
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = true;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
+            // txtRmvEmployee
+            // 
+            this.txtRmvEmployee.Location = new System.Drawing.Point(170, 65);
+            this.txtRmvEmployee.Name = "txtRmvEmployee";
+            this.txtRmvEmployee.Size = new System.Drawing.Size(100, 20);
+            this.txtRmvEmployee.TabIndex = 4;
+            this.txtRmvEmployee.TextChanged += new System.EventHandler(this.txtRmvEmployee_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // EmployeeID
+            // 
+            this.EmployeeID.AutoSize = true;
+            this.EmployeeID.Location = new System.Drawing.Point(102, 68);
+            this.EmployeeID.Name = "EmployeeID";
+            this.EmployeeID.Size = new System.Drawing.Size(70, 13);
+            this.EmployeeID.TabIndex = 6;
+            this.EmployeeID.Text = "Employee ID:";
+            // 
             // ManagerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 297);
+            this.ClientSize = new System.Drawing.Size(282, 193);
+            this.Controls.Add(this.EmployeeID);
+            this.Controls.Add(this.txtRmvEmployee);
             this.Controls.Add(this.btnLogout);
             this.Controls.Add(this.btnModifyEmployee);
             this.Controls.Add(this.btn);
             this.Controls.Add(this.btnCreateEmployee);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ManagerForm";
             this.Text = "ManagerForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -94,5 +128,8 @@
         private System.Windows.Forms.Button btn;
         private System.Windows.Forms.Button btnModifyEmployee;
         private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.TextBox txtRmvEmployee;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label EmployeeID;
     }
 }
