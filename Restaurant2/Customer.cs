533a99081d96c638595cd405dab33d21edf6cbdf
Dice <Dice@BERTHA>
2017-05-03 22:52:25 -0500

    Made changes to Customer class Pay() method with comments for an alternative approach depending on Waiter class approach

--- Restaurant2/Customer.cs
+++ Restaurant2/Customer.cs
@@ -40,44 +40,92 @@ namespace Restaurant2
         //take the payment and subtract to the grouptotal;
         //if they didn't pay enough keep subtrace.
 
-        private decimal groupTotal;
-       
+        // ------------------
 
-        public void Pay (decimal Payment )
+        //private decimal groupTotal;
+        public decimal GroupTotal { get; set; }
+
+        // This method's internal commented code shows a seperate potential way to 
+        // handle the payment logic depending on how the Waiter class operates.
+        public void Pay(decimal payment/*string rawGroupTotal, string rawPayment*/)
         {
-
-            decimal remainingBalance = groupTotal - Payment;
-            //if the customers pay more then they own
-            if(remainingBalance < 0)
+            //decimal payment = 0M;
+            //decimal groupTotal = 0M;
+            //decimal remainingBalance = 0M;
+            //if(decimal.TryParse(rawGroupTotal, out groupTotal))
+            //{
+            //    if(decimal.TryParse(rawPayment, out payment))
+            //    {
+            //        remainingBalance = groupTotal - payment;
+            //        if (remainingBalance < 0M)
+            //        {
+            //            remainingBalance = remainingBalance * -1;
+            //            MessageBox.Show("Your change is $" + remainingBalance.ToString("F2") + "!", "Change");
+            //        }
+            //        else
+            //        {
+            //            groupTotal = remainingBalance;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Payment must only use numeric characters.","Error");
+            //    }
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Group Total must only use numeric characters.", "Error");
+            //}
+            decimal remainingBalance = GroupTotal - payment;
+            if (remainingBalance < 0M)
             {
-
-                MessageBox.Show("You change is " + remainingBalance.ToString());
+                remainingBalance = remainingBalance * -1;
+                MessageBox.Show("Your change is $" + remainingBalance.ToString("F2") + "!", "Change");
             }
-            //if the customers pay less then they own
-            //else if(remainingBalance > 0)
-            //{
-
-            //        remainingBalance = groupTotal - Payment;
-            //        MessageBox.Show("You own " + remainingBalance.ToString());
-
-            //}
-            //else
-
-            //if (Payment >=  groupTotal)
-            //{
-            //        groupTotal = groupTotal - Payment;
-            //        MessageBox.Show("Your change is " + groupTotal );
-                    
-
-            //        //break;
-                
-            //}
-            //else
-            //{
-            //        groupTotal = groupTotal - Payment;
-            //        MessageBox.Show("Thank you");
-                    
-            //}
+            else
+            {
+                GroupTotal = remainingBalance;
+            }
         }
+
+        // ------------------
+
+
+        //public void Pay (decimal Payment )
+        //{
+
+        //    decimal remainingBalance = groupTotal - Payment;
+        //    //if the customers pay more then they own
+        //    if(remainingBalance < 0)
+        //    {
+
+        //        MessageBox.Show("You change is " + remainingBalance.ToString());
+        //    }
+        //    //if the customers pay less then they own
+        //    //else if(remainingBalance > 0)
+        //    //{
+
+        //    //        remainingBalance = groupTotal - Payment;
+        //    //        MessageBox.Show("You own " + remainingBalance.ToString());
+
+        //    //}
+        //    //else
+
+        //    //if (Payment >=  groupTotal)
+        //    //{
+        //    //        groupTotal = groupTotal - Payment;
+        //    //        MessageBox.Show("Your change is " + groupTotal );
+
+
+        //    //        //break;
+
+        //    //}
+        //    //else
+        //    //{
+        //    //        groupTotal = groupTotal - Payment;
+        //    //        MessageBox.Show("Thank you");
+
+        //    //}
+        //}
     }
 }
