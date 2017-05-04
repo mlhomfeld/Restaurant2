aff5cf65ba6eb6e43c980d8d8b2ce61d485174de
Ray <rayjrig@gmail.com>
2017-05-04 12:17:39 -0500

    Copied the refresh button from busboy and added to host.

--- Restaurant2/HostForm.Designer.cs
+++ Restaurant2/HostForm.Designer.cs
@@ -35,14 +35,14 @@
             this.btnTable5 = new System.Windows.Forms.Button();
             this.btnTable6 = new System.Windows.Forms.Button();
             this.btnTable7 = new System.Windows.Forms.Button();
-            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnHostRefresh = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnTable1
             // 
             this.btnTable1.BackColor = System.Drawing.SystemColors.Info;
             this.btnTable1.Location = new System.Drawing.Point(64, 103);
-            this.btnTable1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTable1.Margin = new System.Windows.Forms.Padding(2);
             this.btnTable1.Name = "btnTable1";
             this.btnTable1.Size = new System.Drawing.Size(76, 63);
             this.btnTable1.TabIndex = 0;
@@ -54,7 +54,7 @@
             // 
             this.btnTable2.BackColor = System.Drawing.SystemColors.Info;
             this.btnTable2.Location = new System.Drawing.Point(64, 222);
-            this.btnTable2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTable2.Margin = new System.Windows.Forms.Padding(2);
             this.btnTable2.Name = "btnTable2";
             this.btnTable2.Size = new System.Drawing.Size(76, 66);
             this.btnTable2.TabIndex = 1;
@@ -66,7 +66,7 @@
             // 
             this.btnTable3.BackColor = System.Drawing.SystemColors.Info;
             this.btnTable3.Location = new System.Drawing.Point(210, 103);
-            this.btnTable3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTable3.Margin = new System.Windows.Forms.Padding(2);
             this.btnTable3.Name = "btnTable3";
             this.btnTable3.Size = new System.Drawing.Size(76, 63);
             this.btnTable3.TabIndex = 2;
@@ -78,7 +78,7 @@
             // 
             this.btnTable4.BackColor = System.Drawing.SystemColors.Info;
             this.btnTable4.Location = new System.Drawing.Point(210, 222);
-            this.btnTable4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTable4.Margin = new System.Windows.Forms.Padding(2);
             this.btnTable4.Name = "btnTable4";
             this.btnTable4.Size = new System.Drawing.Size(76, 66);
             this.btnTable4.TabIndex = 3;
@@ -90,7 +90,7 @@
             // 
             this.btnTable5.BackColor = System.Drawing.SystemColors.Info;
             this.btnTable5.Location = new System.Drawing.Point(343, 71);
-            this.btnTable5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTable5.Margin = new System.Windows.Forms.Padding(2);
             this.btnTable5.Name = "btnTable5";
             this.btnTable5.Size = new System.Drawing.Size(70, 65);
             this.btnTable5.TabIndex = 4;
@@ -102,7 +102,7 @@
             // 
             this.btnTable6.BackColor = System.Drawing.SystemColors.Info;
             this.btnTable6.Location = new System.Drawing.Point(343, 173);
-            this.btnTable6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTable6.Margin = new System.Windows.Forms.Padding(2);
             this.btnTable6.Name = "btnTable6";
             this.btnTable6.Size = new System.Drawing.Size(70, 55);
             this.btnTable6.TabIndex = 5;
@@ -114,7 +114,7 @@
             // 
             this.btnTable7.BackColor = System.Drawing.SystemColors.Info;
             this.btnTable7.Location = new System.Drawing.Point(343, 258);
-            this.btnTable7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTable7.Margin = new System.Windows.Forms.Padding(2);
             this.btnTable7.Name = "btnTable7";
             this.btnTable7.Size = new System.Drawing.Size(70, 49);
             this.btnTable7.TabIndex = 6;
@@ -122,15 +122,16 @@
             this.btnTable7.UseVisualStyleBackColor = false;
             this.btnTable7.Click += new System.EventHandler(this.btnTable7_Click);
             // 
-            // btnRefresh
+            // btnHostRefresh
             // 
-            this.btnRefresh.Location = new System.Drawing.Point(452, 352);
-            this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(85, 43);
-            this.btnRefresh.TabIndex = 7;
-            this.btnRefresh.Text = "Refresh";
-            this.btnRefresh.UseVisualStyleBackColor = true;
-            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            this.btnHostRefresh.BackColor = System.Drawing.SystemColors.Info;
+            this.btnHostRefresh.Location = new System.Drawing.Point(449, 367);
+            this.btnHostRefresh.Name = "btnHostRefresh";
+            this.btnHostRefresh.Size = new System.Drawing.Size(89, 41);
+            this.btnHostRefresh.TabIndex = 7;
+            this.btnHostRefresh.Text = "Refresh";
+            this.btnHostRefresh.UseVisualStyleBackColor = false;
+            this.btnHostRefresh.Click += new System.EventHandler(this.btnHostRefresh_Click);
             // 
             // HostForm
             // 
@@ -138,7 +139,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(592, 438);
-            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnHostRefresh);
             this.Controls.Add(this.btnTable7);
             this.Controls.Add(this.btnTable6);
             this.Controls.Add(this.btnTable5);
@@ -146,7 +147,7 @@
             this.Controls.Add(this.btnTable3);
             this.Controls.Add(this.btnTable2);
             this.Controls.Add(this.btnTable1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "HostForm";
             this.Text = "HostForm";
             this.ResumeLayout(false);
@@ -162,6 +163,6 @@
         private System.Windows.Forms.Button btnTable5;
         private System.Windows.Forms.Button btnTable6;
         private System.Windows.Forms.Button btnTable7;
-        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnHostRefresh;
     }
 }
--- Restaurant2/HostForm.cs
+++ Restaurant2/HostForm.cs
@@ -66,14 +66,98 @@ namespace Restaurant2
             hst.SetTableToOccupied(7);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+    
+        private void btnHostRefresh_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void btnRefresh_Click(object sender, EventArgs e)
-        {
-
+            Host bb = new Host();
+            List<string> status = new List<string>();
+            status = bb.RefreshTableStatuses();
+            // Due to scope, had to utilize this with a bunch of switch statements
+            // Wish there was a more elegant way.
+            switch (status[0])
+            {
+                case "Red":
+                    btnTable1.BackColor = Color.Red;
+                    break;
+                case "Green":
+                    btnTable1.BackColor = Color.LightGreen;
+                    break;
+                case "Yellow":
+                    btnTable1.BackColor = Color.Yellow;
+                    break;
+            }
+            switch (status[1])
+            {
+                case "Red":
+                    btnTable2.BackColor = Color.Red;
+                    break;
+                case "Green":
+                    btnTable2.BackColor = Color.LightGreen;
+                    break;
+                case "Yellow":
+                    btnTable2.BackColor = Color.Yellow;
+                    break;
+            }
+            switch (status[2])
+            {
+                case "Red":
+                    btnTable3.BackColor = Color.Red;
+                    break;
+                case "Green":
+                    btnTable3.BackColor = Color.LightGreen;
+                    break;
+                case "Yellow":
+                    btnTable3.BackColor = Color.Yellow;
+                    break;
+            }
+            switch (status[3])
+            {
+                case "Red":
+                    btnTable4.BackColor = Color.Red;
+                    break;
+                case "Green":
+                    btnTable4.BackColor = Color.LightGreen;
+                    break;
+                case "Yellow":
+                    btnTable4.BackColor = Color.Yellow;
+                    break;
+            }
+            switch (status[4])
+            {
+                case "Red":
+                    btnTable5.BackColor = Color.Red;
+                    break;
+                case "Green":
+                    btnTable5.BackColor = Color.LightGreen;
+                    break;
+                case "Yellow":
+                    btnTable5.BackColor = Color.Yellow;
+                    break;
+            }
+            switch (status[5])
+            {
+                case "Red":
+                    btnTable6.BackColor = Color.Red;
+                    break;
+                case "Green":
+                    btnTable6.BackColor = Color.LightGreen;
+                    break;
+                case "Yellow":
+                    btnTable6.BackColor = Color.Yellow;
+                    break;
+            }
+            switch (status[6])
+            {
+                case "Red":
+                    btnTable7.BackColor = Color.Red;
+                    break;
+                case "Green":
+                    btnTable7.BackColor = Color.LightGreen;
+                    break;
+                case "Yellow":
+                    btnTable7.BackColor = Color.Yellow;
+                    break;
+            }
         }
     }
 }
