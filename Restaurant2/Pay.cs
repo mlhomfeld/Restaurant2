211f10548116ad0a0d85e3d3044a1af20ac1a0b3
ray <Rayjrig@gmail.com>
2017-05-06 14:01:23 -0500

    Created a payment Form

---
+++ Restaurant2/Pay.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurant2
+{
+    public partial class Pay : Form
+    {
+        public Pay()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnPayOrder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listYourOrder_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtYourTotal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listOrderedItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBalance_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textTotalPaid_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
