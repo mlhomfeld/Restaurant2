d35464a6a1a3568876724cc89237383f6090687f
monkey <monkey@BB21018>
2017-05-06 14:57:04 -0500

    Made changes to busboy form

--- Restaurant2/BusboyForm.Designer.cs
+++ Restaurant2/BusboyForm.Designer.cs
@@ -42,9 +42,10 @@
             // 
             // btnTable1
             // 
-            this.btnTable1.Location = new System.Drawing.Point(12, 295);
+            this.btnTable1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTable1.Location = new System.Drawing.Point(24, 32);
             this.btnTable1.Name = "btnTable1";
-            this.btnTable1.Size = new System.Drawing.Size(79, 31);
+            this.btnTable1.Size = new System.Drawing.Size(112, 104);
             this.btnTable1.TabIndex = 0;
             this.btnTable1.Text = "Table 1";
             this.btnTable1.UseVisualStyleBackColor = true;
@@ -52,9 +53,10 @@
             // 
             // btnTable3
             // 
-            this.btnTable3.Location = new System.Drawing.Point(12, 398);
+            this.btnTable3.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTable3.Location = new System.Drawing.Point(24, 184);
             this.btnTable3.Name = "btnTable3";
-            this.btnTable3.Size = new System.Drawing.Size(79, 29);
+            this.btnTable3.Size = new System.Drawing.Size(112, 104);
             this.btnTable3.TabIndex = 1;
             this.btnTable3.Text = "Table 3";
             this.btnTable3.UseVisualStyleBackColor = true;
@@ -62,9 +64,10 @@
             // 
             // btnTable5
             // 
-            this.btnTable5.Location = new System.Drawing.Point(492, 342);
+            this.btnTable5.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTable5.Location = new System.Drawing.Point(312, 32);
             this.btnTable5.Name = "btnTable5";
-            this.btnTable5.Size = new System.Drawing.Size(79, 33);
+            this.btnTable5.Size = new System.Drawing.Size(112, 104);
             this.btnTable5.TabIndex = 2;
             this.btnTable5.Text = "Table 5";
             this.btnTable5.UseVisualStyleBackColor = true;
@@ -72,9 +75,10 @@
             // 
             // btnTable2
             // 
-            this.btnTable2.Location = new System.Drawing.Point(12, 342);
+            this.btnTable2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTable2.Location = new System.Drawing.Point(160, 32);
             this.btnTable2.Name = "btnTable2";
-            this.btnTable2.Size = new System.Drawing.Size(79, 33);
+            this.btnTable2.Size = new System.Drawing.Size(112, 104);
             this.btnTable2.TabIndex = 3;
             this.btnTable2.Text = "Table 2";
             this.btnTable2.UseVisualStyleBackColor = true;
@@ -82,9 +86,10 @@
             // 
             // btnTable4
             // 
-            this.btnTable4.Location = new System.Drawing.Point(492, 295);
+            this.btnTable4.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTable4.Location = new System.Drawing.Point(160, 184);
             this.btnTable4.Name = "btnTable4";
-            this.btnTable4.Size = new System.Drawing.Size(79, 31);
+            this.btnTable4.Size = new System.Drawing.Size(112, 104);
             this.btnTable4.TabIndex = 4;
             this.btnTable4.Text = "Table 4";
             this.btnTable4.UseVisualStyleBackColor = true;
@@ -92,9 +97,10 @@
             // 
             // btnTable6
             // 
-            this.btnTable6.Location = new System.Drawing.Point(492, 398);
+            this.btnTable6.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTable6.Location = new System.Drawing.Point(312, 160);
             this.btnTable6.Name = "btnTable6";
-            this.btnTable6.Size = new System.Drawing.Size(79, 29);
+            this.btnTable6.Size = new System.Drawing.Size(112, 104);
             this.btnTable6.TabIndex = 5;
             this.btnTable6.Text = "Table 6";
             this.btnTable6.UseVisualStyleBackColor = true;
@@ -102,9 +108,10 @@
             // 
             // btnTable7
             // 
-            this.btnTable7.Location = new System.Drawing.Point(220, 384);
+            this.btnTable7.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTable7.Location = new System.Drawing.Point(312, 296);
             this.btnTable7.Name = "btnTable7";
-            this.btnTable7.Size = new System.Drawing.Size(179, 56);
+            this.btnTable7.Size = new System.Drawing.Size(112, 104);
             this.btnTable7.TabIndex = 6;
             this.btnTable7.Text = "Table 7";
             this.btnTable7.UseVisualStyleBackColor = true;
@@ -112,6 +119,7 @@
             // 
             // btnBusboyRefresh
             // 
+            this.btnBusboyRefresh.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBusboyRefresh.Location = new System.Drawing.Point(220, 459);
             this.btnBusboyRefresh.Name = "btnBusboyRefresh";
             this.btnBusboyRefresh.Size = new System.Drawing.Size(77, 36);
@@ -122,6 +130,7 @@
             // 
             // btnLogout
             // 
+            this.btnLogout.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogout.Location = new System.Drawing.Point(324, 459);
             this.btnLogout.Margin = new System.Windows.Forms.Padding(2);
             this.btnLogout.Name = "btnLogout";
