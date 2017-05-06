360aec980ef98d547c753e546f22c755dd4d4dc2
daleria <daleria05@gmail.com>
2017-05-06 15:47:01 -0500

    Added color coded labels for table status

--- Restaurant2/BusboyForm.Designer.cs
+++ Restaurant2/BusboyForm.Designer.cs
@@ -38,17 +38,17 @@
             this.btnTable7 = new System.Windows.Forms.Button();
             this.btnBusboyRefresh = new System.Windows.Forms.Button();
             this.btnLogout = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lblTableOpenBusboy = new System.Windows.Forms.Label();
+            this.lblTableOccupiedBusboy = new System.Windows.Forms.Label();
+            this.lblTableDirtyBusboy = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnTable1
             // 
             this.btnTable1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTable1.Location = new System.Drawing.Point(24, 32);
+            this.btnTable1.Location = new System.Drawing.Point(24, 40);
             this.btnTable1.Name = "btnTable1";
-            this.btnTable1.Size = new System.Drawing.Size(112, 104);
+            this.btnTable1.Size = new System.Drawing.Size(104, 88);
             this.btnTable1.TabIndex = 0;
             this.btnTable1.Text = "Table 1";
             this.btnTable1.UseVisualStyleBackColor = true;
@@ -57,9 +57,9 @@
             // btnTable3
             // 
             this.btnTable3.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTable3.Location = new System.Drawing.Point(24, 184);
+            this.btnTable3.Location = new System.Drawing.Point(24, 168);
             this.btnTable3.Name = "btnTable3";
-            this.btnTable3.Size = new System.Drawing.Size(112, 104);
+            this.btnTable3.Size = new System.Drawing.Size(104, 88);
             this.btnTable3.TabIndex = 1;
             this.btnTable3.Text = "Table 3";
             this.btnTable3.UseVisualStyleBackColor = true;
@@ -68,9 +68,9 @@
             // btnTable5
             // 
             this.btnTable5.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTable5.Location = new System.Drawing.Point(312, 32);
+            this.btnTable5.Location = new System.Drawing.Point(288, 24);
             this.btnTable5.Name = "btnTable5";
-            this.btnTable5.Size = new System.Drawing.Size(112, 104);
+            this.btnTable5.Size = new System.Drawing.Size(104, 88);
             this.btnTable5.TabIndex = 2;
             this.btnTable5.Text = "Table 5";
             this.btnTable5.UseVisualStyleBackColor = true;
@@ -79,9 +79,9 @@
             // btnTable2
             // 
             this.btnTable2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTable2.Location = new System.Drawing.Point(160, 32);
+            this.btnTable2.Location = new System.Drawing.Point(152, 40);
             this.btnTable2.Name = "btnTable2";
-            this.btnTable2.Size = new System.Drawing.Size(112, 104);
+            this.btnTable2.Size = new System.Drawing.Size(104, 88);
             this.btnTable2.TabIndex = 3;
             this.btnTable2.Text = "Table 2";
             this.btnTable2.UseVisualStyleBackColor = true;
@@ -90,9 +90,9 @@
             // btnTable4
             // 
             this.btnTable4.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTable4.Location = new System.Drawing.Point(160, 184);
+            this.btnTable4.Location = new System.Drawing.Point(160, 168);
             this.btnTable4.Name = "btnTable4";
-            this.btnTable4.Size = new System.Drawing.Size(112, 104);
+            this.btnTable4.Size = new System.Drawing.Size(104, 88);
             this.btnTable4.TabIndex = 4;
             this.btnTable4.Text = "Table 4";
             this.btnTable4.UseVisualStyleBackColor = true;
@@ -101,9 +101,9 @@
             // btnTable6
             // 
             this.btnTable6.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTable6.Location = new System.Drawing.Point(312, 160);
+            this.btnTable6.Location = new System.Drawing.Point(288, 144);
             this.btnTable6.Name = "btnTable6";
-            this.btnTable6.Size = new System.Drawing.Size(112, 104);
+            this.btnTable6.Size = new System.Drawing.Size(104, 88);
             this.btnTable6.TabIndex = 5;
             this.btnTable6.Text = "Table 6";
             this.btnTable6.UseVisualStyleBackColor = true;
@@ -112,9 +112,9 @@
             // btnTable7
             // 
             this.btnTable7.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTable7.Location = new System.Drawing.Point(312, 296);
+            this.btnTable7.Location = new System.Drawing.Point(288, 256);
             this.btnTable7.Name = "btnTable7";
-            this.btnTable7.Size = new System.Drawing.Size(112, 104);
+            this.btnTable7.Size = new System.Drawing.Size(104, 88);
             this.btnTable7.TabIndex = 6;
             this.btnTable7.Text = "Table 7";
             this.btnTable7.UseVisualStyleBackColor = true;
@@ -143,32 +143,38 @@
             this.btnLogout.UseVisualStyleBackColor = true;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
-            // label1
+            // lblTableOpenBusboy
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(496, 56);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 14);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "label1";
+            this.lblTableOpenBusboy.AutoSize = true;
+            this.lblTableOpenBusboy.BackColor = System.Drawing.Color.Yellow;
+            this.lblTableOpenBusboy.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTableOpenBusboy.Location = new System.Drawing.Point(440, 56);
+            this.lblTableOpenBusboy.Name = "lblTableOpenBusboy";
+            this.lblTableOpenBusboy.Size = new System.Drawing.Size(119, 18);
+            this.lblTableOpenBusboy.TabIndex = 9;
+            this.lblTableOpenBusboy.Text = "TABLE OPEN";
             // 
-            // label2
+            // lblTableOccupiedBusboy
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(496, 104);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 14);
-            this.label2.TabIndex = 10;
-            this.label2.Text = "label2";
+            this.lblTableOccupiedBusboy.AutoSize = true;
+            this.lblTableOccupiedBusboy.BackColor = System.Drawing.Color.LimeGreen;
+            this.lblTableOccupiedBusboy.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTableOccupiedBusboy.Location = new System.Drawing.Point(440, 96);
+            this.lblTableOccupiedBusboy.Name = "lblTableOccupiedBusboy";
+            this.lblTableOccupiedBusboy.Size = new System.Drawing.Size(160, 18);
+            this.lblTableOccupiedBusboy.TabIndex = 10;
+            this.lblTableOccupiedBusboy.Text = "TABLE OCCUPIED";
             // 
-            // label3
+            // lblTableDirtyBusboy
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(496, 152);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 14);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "label3";
+            this.lblTableDirtyBusboy.AutoSize = true;
+            this.lblTableDirtyBusboy.BackColor = System.Drawing.Color.Red;
+            this.lblTableDirtyBusboy.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTableDirtyBusboy.Location = new System.Drawing.Point(440, 136);
+            this.lblTableDirtyBusboy.Name = "lblTableDirtyBusboy";
+            this.lblTableDirtyBusboy.Size = new System.Drawing.Size(130, 18);
+            this.lblTableDirtyBusboy.TabIndex = 11;
+            this.lblTableDirtyBusboy.Text = "TABLE DIRTY";
             // 
             // BusboyForm
             // 
@@ -176,9 +182,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(599, 517);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTableDirtyBusboy);
+            this.Controls.Add(this.lblTableOccupiedBusboy);
+            this.Controls.Add(this.lblTableOpenBusboy);
             this.Controls.Add(this.btnLogout);
             this.Controls.Add(this.btnBusboyRefresh);
             this.Controls.Add(this.btnTable7);
@@ -209,8 +215,8 @@
         private System.Windows.Forms.Button btnTable7;
         private System.Windows.Forms.Button btnBusboyRefresh;
         private System.Windows.Forms.Button btnLogout;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTableOpenBusboy;
+        private System.Windows.Forms.Label lblTableOccupiedBusboy;
+        private System.Windows.Forms.Label lblTableDirtyBusboy;
     }
 }
