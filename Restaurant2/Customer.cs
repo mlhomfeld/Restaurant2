9eb3b70990a1b439eaad2bd91b317b0e8eca339a
mlhomfeld <mlhomfeld@yahoo.com>
2017-05-09 17:36:13 -0500

    Documentation edits

--- Restaurant2/Customer.cs
+++ Restaurant2/Customer.cs
@@ -12,11 +12,9 @@ namespace Restaurant2
     public class Customer
     {
 
-        //public decimal GroupTotal { get; set; }
 
-        // This method's internal commented code shows a seperate potential way to 
-        // handle the payment logic depending on how the Waiter class operates.
-        public string Pay(/*decimal payment*/string rawGroupTotal, string rawPayment)
+        // This method is used on the Pay form for the Pay button logic
+        public string Pay(string rawGroupTotal, string rawPayment)
         {
             decimal payment = 0M;
             decimal groupTotal = 0M;
@@ -47,16 +45,6 @@ namespace Restaurant2
                 MessageBox.Show("Group Total must only use numeric characters.", "Error");
             }
             return remainingBalance.ToString();
-            //decimal remainingBalance = GroupTotal - payment;
-            //if (remainingBalance < 0M)
-            //{
-            //    remainingBalance = remainingBalance * -1;
-            //    MessageBox.Show("Your change is $" + remainingBalance.ToString("F2") + "!", "Change");
-            //}
-            //else
-            //{
-            //    GroupTotal = remainingBalance;
-            //}
         }
     }
 }
